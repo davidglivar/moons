09cba8477f7919860987980ae20777b80bb10985
David Glivar <davidglivar@gmail.com>
2014-06-03 16:04:48 -0700

    jshintrc, some more tests

--- test/core.test.js
+++ test/core.test.js
@@ -7,6 +7,17 @@ describe('core', function () {
     expect(core).to.be.an('object');
   });
 
+  describe('#cAF()', function () {
+    
+    it('exists', function () {
+      expect(core).to.have.property('cAF');
+      expect(core.cAF).to.be.a('function');
+      expect(core.cAF.length).to.be(1);
+    });
+
+    it('write some tests');
+  });
+
   describe('#prop()', function () {
     
     it('exists', function () {
@@ -18,5 +29,16 @@ describe('core', function () {
     it('returns null for an invalid property', function () {
       expect(core.prop('notrealatall')).to.be(null);
     });
+  });
+
+  describe('#rAF()', function () {
+
+    it('exists', function () {
+      expect(core).to.have.property('rAF');
+      expect(core.rAF).to.be.a('function');
+      expect(core.rAF.length).to.be(1);
+    });
+
+    it('write some tests');
   });
 });
