e0c44238d4fe18f0a6b5126f7d2471645e423ae1
David Glivar <davidglivar@gmail.com>
2014-06-04 16:49:30 -0700

    debouncer test stubs

---
+++ test/debouncer.test.js
@@ -0,0 +1,44 @@
+var Debouncer = require('../moons/debouncer');
+
+describe('Debouncer', function () {
+  
+  it('is a function', function () {
+    expect(Debouncer).to.be.a('function');
+  });
+
+  describe('#_requestTick()', function () {
+    it('write some tests');
+  });
+
+  describe('#_update()', function () {
+    it('write some tests');
+  });
+
+  describe('#attach()', function () {
+    it('write some tests');
+  });
+
+  describe('#detach()', function () {
+    it('write some tests');
+  });
+
+  describe('#detachAll()', function () {
+    it('write some tests');
+  });
+
+  describe('#exists()', function () {
+    it('write some tests');
+  });
+
+  describe('#listen()', function () {
+    it('write some tests');
+  });
+
+  describe('#runStack()', function () {
+    it('write some tests');
+  });
+
+  describe('#stop()', function () {
+    it('write some tests');
+  });
+});
