87f87c76f113fc9697591a2b45b0cdadf314be0b
David Glivar <davidglivar@gmail.com>
2014-06-10 13:05:18 -0700

    changing renderer to an object instead of a constructor

--- moons/renderer.js
+++ moons/renderer.js
@@ -5,78 +5,63 @@
  */
 var core = require('./core');
 
-function Renderer(opts) {
-  opts = opts || {};
+var renderer = {
 
-  if (typeof Renderer.instance === 'object') {
-    return Renderer.instance;
+  _calls: [],
+
+  _rid: null,
+
+  _stopped: false,
+
+  add: function (func) {
+    if (typeof func !== 'function') {
+      throw new TypeError('Expected a function, got ' + typeof func);
+    }
+    if (this.exists(func)) return false;
+    this._calls.push(func);
+    return true;
+  },
+
+  exists: function (func) {
+    if (typeof func !== 'function') {
+      throw new TypeError('Expected a function, got ' + typeof func);
+    }
+    return this._calls.indexOf(func) >= 0;
+  },
+
+  remove: function (func) {
+    if (typeof func !== 'function') {
+      throw new TypeError('Expected a function, got ' + typeof func);
+    }
+    var idx = this._calls.indexOf(func);
+    if (idx < 0) return false;
+    this._calls.splice(idx, 1);
+    return true;
+  },
+
+  start: function () {
+    if (!this._stopped) return false;
+    this._stopped = false;
+    this._rid = core.rAF(this.update.bind(this));
+    return true;
+  },
+
+  stop: function () {
+    if (this._stopped) return false;
+    this._stopped = true;
+    core.cAF(this._rid);
+    return true;
+  },
+
+  update: function () {
+    if (this._stopped) return;
+    var i = 0
+      , l = this._calls.length;
+    for (i; i < l; i++) {
+      this._calls[i]();
+    }
+    core.rAF(this.update);
   }
-
-  this._calls = [];
-
-  this._rid = null;
-
-  this._stopped = false;
-
-  this.update = this.update.bind(this);
-}
-
-Renderer.prototype.exists = function (func) {
-  if (typeof func !== 'function') {
-    throw new TypeError('Expected a function, got ' + typeof func);
-  }
-  return this._calls.indexOf(func) >= 0;
 };
 
-Renderer.prototype.update = function () {
-  if (this._stopped) return;
-  var i = 0
-    , l = this._calls.length;
-  for (i; i < l; i++) {
-    this._calls[i]();
-  }
-  core.rAF(this.update);
-};
-
-Renderer.instance = undefined;
-
-Renderer.add = function (func) {
-  if (typeof func !== 'function') {
-    throw new TypeError('Expected a function, got ' + typeof func);
-  }
-  if (Renderer.instance.exists(func)) return false;
-  Renderer.instance._calls.push(func);
-  return Renderer.instance;
-};
-
-Renderer.create = function (opts) {
-  Renderer.instance = new Renderer(opts);
-  return Renderer.instance;
-};
-
-Renderer.destroy = function () {
-  Renderer.stop();
-  return Renderer.instance = undefined;
-};
-
-Renderer.remove = function (func) {
-  if (typeof func !== 'function') {
-    throw new TypeError('Expected a function, got ' + typeof func);
-  }
-  var idx = Renderer.instance._calls.indexOf(func);
-  if (idx < 0) return false;
-  Renderer.instance._calls.splice(idx, 1);
-  return Renderer.instance;
-};
-
-Renderer.start = function () {
-  Renderer.instance._stopped = false;
-  Renderer.instance._rid = core.rAF(Renderer.instance.update);
-};
-
-Renderer.stop = function () {
-  Renderer.instance._stopped = true;
-  core.cAF(Renderer.instance._rid);
-};
-
-module.exports = Renderer;
+module.exports = renderer;
--- test/renderer.test.js
+++ test/renderer.test.js
@@ -1,54 +1,56 @@
-var Renderer = require('../moons/renderer');
+var renderer = require('../moons/renderer');
 
-describe('Renderer', function () {
+describe('renderer', function () {
   
-  it('is a function', function () {
-    expect(Renderer).to.be.a('function');
+  it('is an object');
+
+  describe('_calls', function () {
+    it('is an array');
   });
 
-  describe('instance methods/props', function () {
-    
-    describe('#exists()', function () {
-      it('write some tests');
-    });
-
-    describe('#update()', function () {
-      it('write some tests');
-    });
+  describe('_rid', function () {
+    it('is a number');
   });
 
-  describe('static methods/props', function () {
+  describe('_stopped', function () {
+    it('is a boolean');
+  });
 
-    describe('instance', function () {
-      
-      it('exists and is undefined', function () {
-        expect(Renderer).to.have.property('instance');
-        expect(typeof Renderer.instance).to.be('undefined');
-      });
-    });
+  describe('#add()', function () {
+    it('throws a TypeError if you pass anything other than a function');
+    it('adds a function to the call stack');
+    it('does not add a function that already exists in the stack');
+    it('returns a boolean');
+  });
 
-    describe('#add()', function () {
-      it('write some tests');
-    });
+  describe('#exists()', function () {
+    it('throws a TypeError if you pass anything other than a function');
+    it('returns true if a function is in the call stack');
+    it('returns false if a function is not in the call stack');
+  });
 
-    describe('#create()', function () {
-      it('write some tests');
-    });
+  describe('#remove()', function () {
+    it('throws a TypeError if you pass anything other than a function');
+    it('returns false if the passed function is not found in the call stack');
+    it('returns true for a function that is found and removed from the call stack');
+  });
 
-    describe('#destroy()', function () {
-      it('write some tests');
-    });
+  describe('#start()', function () {
+    it('returns false if the renderer is already started');
+    it('returns true on successful start');
+    it('flips renderer._stopped to false');
+    it('stores the requestAnimationFrame id in renderer._rid');
+  });
 
-    describe('#remove()', function () {
-      it('write some tests');
-    });
+  describe('#stop()', function () {
+    it('returns false if the renderer has already stopped');
+    it('returns true on a successful stop');
+    it('flips renderer._stopped to true');
+    it('cancels the renderer._rid requestAnimationFrame id');
+  });
 
-    describe('#start()', function () {
-      it('write some tests');
-    });
-
-    describe('#stop()', function () {
-      it('write some tests');
-    });
+  describe('#update()', function () {
+    it('does not loop through the call stack if the renderer is stopped');
+    it('calls each function in the call stack');
   });
 });
